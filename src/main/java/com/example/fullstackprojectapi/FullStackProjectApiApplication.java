12952a3e776773202010c01705a99f56fd279e84
nikko <nikkorivas@outlook.com>
2022-09-02 12:30:46 +0100

    created spring boilerplate

---
+++ src/main/java/com/example/fullstackprojectapi/FullStackProjectApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.fullstackprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProjectApiApplication.class, args);
+	}
+
+}
+
+
